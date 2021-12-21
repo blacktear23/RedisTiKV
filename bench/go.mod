5e70dd894b8162062758934e5a596d5d45d72e8a
yulai.li <blacktear23@gmail.com>
2021-12-21 13:42:44 +0800

    Add golang benchmark code

---
+++ bench/go.mod
@@ -0,0 +1,8 @@
+module bench
+
+go 1.17
+
+require (
+	github.com/garyburd/redigo v1.6.3 // indirect
+	github.com/gomodule/redigo v1.8.6 // indirect
+)
---
+++ bench/main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"sync"
+	"time"
+
+	"github.com/garyburd/redigo/redis"
+)
+
+type Job struct {
+	Name string
+	Args []interface{}
+	Wg   *sync.WaitGroup
+}
+
+type worker struct {
+	conn  redis.Conn
+	queue chan Job
+}
+
+func (w worker) Run() {
+	for job := range w.queue {
+		w.conn.Do(job.Name, job.Args...)
+		job.Wg.Done()
+	}
+}
+
+func main() {
+	pool := &redis.Pool{
+		// Other pool configuration not shown in this example.
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.Dial("tcp", "127.0.0.1:6379")
+			if err != nil {
+				return nil, err
+			}
+			return c, nil
+		},
+	}
+
+	queue := make(chan Job, 100)
+
+	for i := 0; i < 50; i++ {
+		w := worker{conn: pool.Get(), queue: queue}
+		go w.Run()
+	}
+
+	conn := pool.Get()
+	fmt.Printf("Run tikv.put 10000\n")
+	start_at := time.Now()
+	wg := sync.WaitGroup{}
+	wg.Add(10000)
+	for i := 0; i < 10000; i++ {
+		key := "pkey-" + strconv.Itoa(i)
+		value := "pvaluedata-" + strconv.Itoa(i) + "-123456789809"
+		queue <- Job{
+			Name: "tikv.put",
+			Args: []interface{}{key, value},
+			Wg:   &wg,
+		}
+	}
+	wg.Wait()
+	end_at := time.Now()
+	fmt.Printf("Time: %v\n", end_at.Sub(start_at))
+
+	fmt.Printf("Run tikv.get 10000\n")
+	start_at = time.Now()
+	wg = sync.WaitGroup{}
+	wg.Add(10000)
+	for i := 0; i < 10000; i++ {
+		key := "pkey-" + strconv.Itoa(i)
+		queue <- Job{
+			Name: "tikv.get",
+			Args: []interface{}{key},
+			Wg:   &wg,
+		}
+	}
+	wg.Wait()
+	end_at = time.Now()
+	fmt.Printf("Time: %v\n", end_at.Sub(start_at))
+
+	fmt.Printf("Run set 10000\n")
+	start_at = time.Now()
+	for i := 0; i < 10000; i++ {
+		key := "key-" + strconv.Itoa(i)
+		value := "valuedata-" + strconv.Itoa(i) + "-123456789809"
+		conn.Do("set", key, value)
+	}
+	end_at = time.Now()
+	fmt.Printf("Time: %v\n", end_at.Sub(start_at))
+
+	fmt.Printf("Run get 10000\n")
+	start_at = time.Now()
+	for i := 0; i < 10000; i++ {
+		key := "key-" + strconv.Itoa(i)
+		conn.Do("get", key)
+	}
+	end_at = time.Now()
+	fmt.Printf("Time: %v\n", end_at.Sub(start_at))
+}
